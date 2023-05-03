1ac8969f3e84b7a4ad50e9d34a0145133bf573d0
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-05-03 15:32:12 +0200

    we are finally connected yuhu lets rewrite all the code lets fucking gooooo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// server/index.js
-
+import http from 'http'
 import express from 'express'
-import bodyParser from 'body-parser'
+import { Server } from 'socket.io'
 
 import * as auth from './authentication/index.js'
 import * as ranking from './ranking/index.js'
@@ -10,53 +9,24 @@ import { handleHttpResult } from './helpers/index.js'
 const PORT = process.env.PORT || 3001
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server)//, { cors: { origin: '*' } })
 
-app.use(bodyParser.json())
-
-app.listen(PORT, () => { // This is needed to bind to the socket and listen
-  console.log('HELLO MY DUDES!')
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
 })
 
-app.get('/', (req, res) => {
-  res.json({ message: 'mafia-clicker', version: '1.0.0' })
-})
+io.on('connection', (socket) => {
+  console.log('a user connected')
 
-app.post('/auth', (req, res) => {
-  const result = auth.authenticate(req.body)
-  handleHttpResult(res, result)
-})
+  socket.emit('howareyou', 'finetho')
 
-app.post('/user', (req, res) => {
-  const result = auth.getUserData(req.body)
-  handleHttpResult(res, result)
-})
+  socket.on('hello', (data) => {
+    console.log(data, JSON.parse(data))
+    socket.emit('howareyou')
+  })
 
-app.get('/users', (req, res) => {
-  const result = auth.getAllUsersData()
-  handleHttpResult(res, result)
-})
-
-app.post('/newUser', (req, res) => {
-  const result = auth.createNewUser(req.body)
-  handleHttpResult(res, result)
-})
-
-app.put('/changeUsername', (req, res) => {
-  const result = auth.updateUsername(req.body)
-  handleHttpResult(res, result)
-})
-
-app.get('/getRanking', (req, res) => {
-  const result = ranking.getWholeRanking(req.body)
-  handleHttpResult(res, result)
-})
-
-app.put('/updateScore', (req, res) => {
-  const result = ranking.updatePosition(req.body)
-  handleHttpResult(res, result)
-})
-
-app.delete('/deleteScore', (req, res) => {
-  const result = ranking.deletePositionScore(req.body)
-  handleHttpResult(res, result)
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 })
